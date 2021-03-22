5e891ebf995bc4ec48d2ea0c2ccc43dd2fe65676
kimxons <kitongameshack9@gmail.com>
2021-03-22 19:56:07 +0300

    added market trades and OHLCV

--- price.py
+++ price.py
@@ -1,4 +1,5 @@
 import ccxt
+import time
 
 def getprice(symbol):
     symbol = input(symbol.upper()) #USDT/BTC
@@ -14,6 +15,16 @@ def getprice(symbol):
             # print(exchange.fetch_tickers())
             print(exchange.fetch_tickers(['ETH/BTC'])) #fetch minimal data on ETH
 
+        #using fetchOHLCV / fetch_ohlcv to get the list of OHLCV candles for a particular symbol
+        if (exchange.has['fetchOHLCV']):
+            for symbol in exchange.markets:
+                time.sleep(exchange.rateLimit / 1000)
+                print(symbol, exchange.fetch_ohlcv(symbol, '1m')) #one minute
+                
+        if(exchange.has['fetchTrades']):
+            for symbol in exchange.markets:
+                print(symbol, exchange.fetch_trades(symbol))
+
         price = exchange.fetch_ticker(symbol)
         fprice = price['info']['lastPrice']
         if(sep[1] == "USD" or sep[1] == 'USDT'):
@@ -25,5 +36,4 @@ def getprice(symbol):
     except (ccxt.ExchangeError, ccxt.NetworkError) as error:
         return 'Found an error', type(error).__name__, error.args
     raise
-
 print(getprice("Enter market symbol: "))
